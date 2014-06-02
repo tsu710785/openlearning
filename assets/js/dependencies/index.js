3dbe618654283a25d70e74f9338a16da4d92d0be
imet3 <imetliao@gmail.com>
2014-06-03 03:15:10 +0800

    views

--- assets/js/dependencies/index.js
+++ assets/js/dependencies/index.js
@@ -9,4 +9,3 @@ $(document).ready(function(){
     //   "channelmode=no,directories=no,fullscreen=no,location=no,menubar=no,resizeable=yes,scrollbars=yes,status=no,toolbar=no,left=400,top=200,width=700,height=450", 'false');
     // });
 });
-
