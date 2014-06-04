93e650c758aa9472ebf7376fae9d7c0e09ec4808
imet3 <imetliao@gmail.com>
2014-06-04 11:37:56 +0800

    controller

---
+++ api/controllers/DiscussController.js
@@ -0,0 +1,24 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side logic for managing mains
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+   
+  list: function (req, res) {
+
+    // Send a JSON response
+
+    Main
+    .find({})
+    .exec(function (err, mains) {
+      return res.view("discuss", {
+        title: "discuss"
+      });
+    });
+  },
+  
+};
+
